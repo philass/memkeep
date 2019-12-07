0b07369cf9ccbc15d64c40b4a094c4dba2753c61
Philip Lassen <philiplassen@gmail.com>
2019-12-07 05:29:58 +0100

    Added functionality to add questions to a file

---
+++ src/Add.hs
@@ -0,0 +1,20 @@
+module Add where
+
+import System.IO
+import System.Directory
+
+appendFileLine file line = appendFile file $ line ++ ['\n']
+addQuestion = do
+  let file = "qa.db"
+  fileExist <- doesFileExist file
+  if (not fileExist) then  (putStrLn "qa.db was created") 
+                     else pure()
+  putStrLn "What is the question?"
+  question <- getLine
+  putStrLn "What is the answer?"
+  answer <- getLine
+  appendFileLine file question
+  appendFileLine file answer
+  putStrLn "Question Added"
+
+
--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,13 @@
 module Main where
 
+import Add
+import System.Environment
+
 main :: IO ()
 main = do
   putStrLn "hello world"
+  args <- getArgs
+  let firstArg =  head args
+  if (firstArg == "add") then addQuestion
+                          else pure()
+
