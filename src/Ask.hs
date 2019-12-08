b55f12aa990183a86503b3676d44eb13ee1c2260
Philip Lassen <philiplassen@gmail.com>
2019-12-08 04:56:08 +0100

    Added functionality for asking questions

---
+++ src/Ask.hs
@@ -0,0 +1,28 @@
+module Ask where
+
+import System.IO
+import System.Directory
+import System.Random
+import Data.Typeable
+appendFileLine :: FilePath -> String -> IO ()
+appendFileLine file line = appendFile file $ line ++ ['\n']
+
+askQuestion :: IO ()
+askQuestion = do
+  let file = "qa.db"
+  fileExist <- doesFileExist file
+  if (not fileExist) then  (putStrLn "No File with Questions") 
+                     else do 
+                            h <- openFile file ReadMode
+                            s <- hGetContents h
+                            let fileLines = lines s
+                            let numLines = length fileLines
+                            randomRow <- getStdRandom $ randomR $ (0, (numLines `div` 2) - 1)
+                            putStrLn $ "The number of Lines : " ++ show numLines
+                            putStrLn $ fileLines !! (randomRow * 2)
+                            userAnswer <- getLine
+                            let actualAnswer = fileLines !! (randomRow * 2 + 1)
+                            if actualAnswer == userAnswer then putStrLn "Correct!"
+                                                          else putStrLn $ "Wrong! the answer is : " ++ actualAnswer
+
+
--- src/Main.hs
+++ src/Main.hs
@@ -1,13 +1,14 @@
 module Main where
 
 import Add
+import Ask
 import System.Environment
+
 
 main :: IO ()
 main = do
   putStrLn "hello world"
   args <- getArgs
   let firstArg =  head args
-  if (firstArg == "add") then addQuestion
-                          else pure()
-
+  case firstArg of "add" -> addQuestion
+                   "ask" -> askQuestion
